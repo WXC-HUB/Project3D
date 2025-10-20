e68660a9c34f7fe473ca663be7792362a731818c
WXC-HUB <54668505+WXC-HUB@users.noreply.github.com>
2025-10-20 17:58:05 +0800

    LeveGridObj继承自CharacterCtrlBase

--- Assets/Scripts/GamePlay/LevelGrid/LevelGridTileObject.cs
+++ Assets/Scripts/GamePlay/LevelGrid/LevelGridTileObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelGridTileObject : MonoBehaviour
+public class LevelGridTileObject : CharacterCtrlBase
 {
     // Start is called before the first frame update
     void Start()
